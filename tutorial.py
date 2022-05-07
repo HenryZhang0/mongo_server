8cb811bd0bbaa4f72bb23718315f8302ac345224
Henry Zhang <zhangyiheng2003@gmail.com>
2022-05-06 21:23:44 -0400

    first

---
+++ tutorial.py
@@ -0,0 +1,13 @@
+import ssl
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://pog:321lester@cluster-0.zslaw.mongodb.net/test")
+
+
+db = cluster["tiktok"]
+collection = db["userdata"]
+
+post = {"name": "henry", "score" : 5}
+
+collection.insert_one(post)
