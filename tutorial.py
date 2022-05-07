d1df77c771288d95978ee2ec5e20c8071c85e916
ryannhu <92134792+ryannhu@users.noreply.github.com>
2022-05-06 21:44:09 -0400

    carried

--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,4 @@
+from re import L
 import ssl
 import pymongo
 from pymongo import MongoClient
@@ -9,5 +10,16 @@ db = cluster["tiktok"]
 collection = db["userdata"]
 
 post = {"name": "henry", "score" : 5}
+post1 = {'name': "joe", "score" : 10}
+post2 = {'name': "ryan", "score" : 99}
+post3 = {'name': "tim", "score" : 1}
+# collection.insert_many([post, post1])
 
-collection.insert_one(post)
+# results = collection.insert_many([post, post1, post2, post3])
+# results = collection.find({"name": "e"})
+results = collection.update_many({"name": "jim"}, {"$set": {"score": -2}}) # you can also add fields with update
+
+results = collection.update_many({"name": "henry"}, {"$inc": {"looks": -100}})
+
+post_count = collection.count_documents({})
+print(post_count)
